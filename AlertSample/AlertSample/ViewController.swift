baab283826494fbd2bff128b3e511ccebcf9baed
bamssong <seungjin.an@nestberg.com>
2015-08-20 00:59:22 +0900

    다이얼로그 샘플 추가.

---
+++ AlertSample/AlertSample/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  AlertSample
+//
+//  Created by anseungjin on 2015. 8. 19..
+//  Copyright (c) 2015년 bamssong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func clinckBasicAlertViewStyle(sender: AnyObject) {
+        let dialog = UIAlertController(title: "title", message: "Basic AlertView Style", preferredStyle: UIAlertControllerStyle.Alert)
+        let cancelAction = UIAlertAction(title: "cancel", style: UIAlertActionStyle.Cancel, handler: {(action) -> Void in println("cancel")})
+        let okAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.Default, handler: {(action) -> Void in println("ok")})
+
+        dialog.addAction(okAction)
+        dialog.addAction(cancelAction)
+
+        
+        self.presentViewController(dialog, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func clickAlertStyleWithTextField(sender: AnyObject) {
+        let dialog = UIAlertController(title: "title", message: "AlertSytle with TextField", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let cancelAction = UIAlertAction(title: "cancel", style: UIAlertActionStyle.Cancel, handler: {(action) -> Void in println("cancel")})
+        let okAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.Default, handler: {(action) -> Void in
+            let userInput = dialog.textFields![0] as! UITextField
+            self.label.text = userInput.text
+            println("ok")})
+        
+        
+        func addTextField(textField: UITextField!){
+            textField.keyboardType = UIKeyboardType.NumberPad
+        }
+        
+        dialog.addAction(okAction)
+        dialog.addAction(cancelAction)
+        dialog.addTextFieldWithConfigurationHandler(addTextField)
+    
+        self.presentViewController(dialog, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func clickBasicActionsheetStyle(sender: AnyObject) {
+        let dialog = UIAlertController(title: "title", message: "Basic AlertView Style", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        let cancelAction = UIAlertAction(title: "cancel", style: UIAlertActionStyle.Cancel, handler: {(action) -> Void in println("cancel")})
+        let okAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.Default, handler: {(action) -> Void in println("ok")})
+
+        let otherAction = UIAlertAction(title: "otherAction", style: UIAlertActionStyle.Destructive, handler: {(action) -> Void in println("ok")})
+
+        dialog.addAction(okAction)
+        dialog.addAction(cancelAction)
+        dialog.addAction(otherAction)
+        
+        self.presentViewController(dialog, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
